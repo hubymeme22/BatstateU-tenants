dc4cdac676b0dbdc07bb714a6f8fbc39f67f235e
Sanjero20 <gelosantos72@gmail.com>
2023-03-08 00:59:07 +0800

    Add admin login page

--- src/pages/admin/login/index.jsx
+++ src/pages/admin/login/index.jsx
@@ -1,7 +1,76 @@
 import React from 'react';
+import { useAuth } from '../../../hooks/useAuth';
+import useInput from '../../../hooks/useInput';
+
+import {
+  LoginContainer,
+  Form,
+  Title,
+  Field,
+  Label,
+  Input,
+  Button,
+  Link,
+  UserIcon,
+  KeyIcon,
+} from './styled';
 
 function AdminLogin() {
-  return <div>Admin Login page</div>;
+  const [SRCode, SRCodeHandler, resetSRCode] = useInput('');
+  const [password, passwordHandler, resetPassword] = useInput('');
+
+  const auth = useAuth();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    // if valid, login
+    //    auth.login();
+    // else
+    //    show error popup modal
+  };
+
+  return (
+    <LoginContainer>
+      <Form onSubmit={submitForm}>
+        <Title>ADMIN LOG IN</Title>
+
+        <Field>
+          <Label htmlFor="sr-code" component={'adfdsf'}>
+            Username
+          </Label>
+
+          <div>
+            <Input
+              type="text"
+              id="sr-code"
+              placeholder="Username"
+              {...SRCodeHandler}
+            />
+            <UserIcon />
+          </div>
+        </Field>
+
+        <Field>
+          <Label htmlFor="password">Password</Label>
+
+          <div>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Password"
+              {...passwordHandler}
+            />
+            <KeyIcon />
+          </div>
+        </Field>
+
+        <Link>Forget Password?</Link>
+
+        <Button onClick={auth.login}> LOGIN </Button>
+      </Form>
+    </LoginContainer>
+  );
 }
 
 export default AdminLogin;
---
+++ src/pages/admin/login/styled.jsx
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+import { Link as LinkTag } from 'react-router-dom';
+
+// Icons
+import { FaUserCircle } from 'react-icons/fa';
+import { BsKey } from 'react-icons/bs';
+
+export const LoginContainer = styled.div`
+  background-color: ${({ theme }) => theme.darkGray};
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Form = styled.form`
+  background-color: ${({ theme }) => theme.primary};
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 2rem;
+  border-radius: 25px;
+`;
+
+export const Title = styled.h1`
+  font-family: 'Staatliches';
+  color: white;
+  font-size: 2rem;
+`;
+
+export const Field = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div {
+    display: flex;
+    position: relative;
+
+    svg {
+      font-size: 1.5rem;
+      position: absolute;
+      align-self: center;
+      right: 0.5rem;
+      color: gray;
+    }
+  }
+`;
+
+export const Label = styled.label`
+  color: white;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 0.5rem;
+  padding-right: 2rem;
+  border-radius: 5px;
+  border: 0;
+`;
+
+export const Button = styled.button`
+  background-color: ${({ theme }) => theme.lightRed};
+  color: white;
+  padding: 0.25rem;
+  border: 0;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const Link = styled(LinkTag)`
+  font-size: 0.9rem;
+  color: white;
+  text-decoration: underline;
+`;
+
+export const UserIcon = styled(FaUserCircle)``;
+
+export const KeyIcon = styled(BsKey)`
+  transform: rotate(90deg);
+`;
