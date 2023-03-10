832a1a48d6171b789a5fe27c8d43a8de180c5467
MrAgui <karlaguilar3800@gmail.com>
2023-03-10 19:44:47 +0800

    under develop deleteBilling

---
+++ server/api/routes/billing/deleteBilling.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { deleteRequestPermission, setJSONPacketFormat } from "../../../middleware/tokenValidator.js";
+import { AdminMongoDBConnection } from "../../../modules/AdminDBConnection.js";
+// import paramChecker from "../../../modules/paramchecker.js";
+
+const deleteBilling = Router();
+
+setJSONPacketFormat({error: '', deleted: false});
+deleteBilling.delete('/:slot/:username', deleteRequestPermission, (req, res) => {
+    const responseFormat = { deleted: [], error: '' };
+    const adminDatabase = new AdminMongoDBConnection(req.allowedData);
+
+    adminDatabase.setAcceptCallback(userdata => {
+        responseFormat.deleted = userdata;
+        res.json(responseFormat);
+    });
+
+    adminDatabase.setRejectCallback(error => {
+        responseFormat.error = error;
+        res.json(responseFormat);
+    });
+
+    adminDatabase.deleteUserBill(req.params.slot, req.params.username);
+});
+
+export default deleteBilling;
