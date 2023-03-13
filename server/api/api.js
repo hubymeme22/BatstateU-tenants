a6c796870b6f3c4b5878aa0c316af92f600bdc5c
MrAgui <karlaguilar3800@gmail.com>
2023-03-13 13:55:41 +0800

    restructure imports, add deleteStudentRoom route

--- server/api/api.js
+++ server/api/api.js
@@ -2,14 +2,21 @@ import { Router } from "express";
 
 import register from "./routes/register.js";
 import login from "./routes/login.js";
+import forgotPassword from "./routes/forgotpass.js";
+
+
 import getUnits from "./routes/slots/getUnits.js";
 import addUnit from "./routes/slots/addUnits.js";
+
 import addBilling from "./routes/billing/addBilling.js";
-import verify from "./routes/students/verify.js";
 import getBillings from "./routes/billing/getBilling.js";
-import addStudentRoom from "./routes/students/addRoom.js";
-import forgotPassword from "./routes/forgotpass.js";
 import deleteBilling from "./routes/billing/deleteBilling.js";
+
+import verify from "./routes/students/verify.js";
+import addStudentRoom from "./routes/students/addRoom.js";
+import deleteStudentRoom from "./routes/students/deleteRoom.js";
+
+
 
 const api = Router();
 api.use("/register", register);
@@ -19,6 +26,7 @@ api.use("/forgotpass", forgotPassword);
 // units crud operations
 api.use("/slots", getUnits);
 api.use("/slots", addUnit);
+api.use("/slots", deleteUnits)
 
 // bill crud operations
 api.use("/billing", addBilling);
@@ -28,5 +36,6 @@ api.use("/billing", deleteBilling);
 // students crud operations
 api.use("/students", verify);
 api.use("/students", addStudentRoom);
+api.use("/student", deleteStudentRoom);
 
 export default api;
