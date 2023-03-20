600e5aac486c91a75f9540063dbd5ff5c07175e1
Sanjero20 <gelosantos72@gmail.com>
2023-03-20 22:18:13 +0800

    Applied cors to the whole application

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import 'dotenv/config';
 
 import api from './api/api.js';
@@ -10,8 +11,14 @@ const app = express();
 const appIP = process.env.IP;
 const appPort = process.env.PORT;
 const mongodbURI = process.env.MONGODB_URI;
+const corsOptions = {
+  origin: process.env.ACCEPTED_URIS,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use('/api', api);
 app.use('/', uiConnector);
@@ -19,15 +26,16 @@ app.use('/', uiConnector);
 console.log('[*] Connecting to database...');
 
 mongoose.set('strictQuery', true);
-mongoose.connect(mongodbURI)
-    .then(() => {
-        console.log('[+] Connected to database!');
-        app.listen(appPort, appIP, () => {
-            console.log(`[+] Server started at: http://${appIP}:${appPort}/`);
-        });
-    })
-
-    .catch((error) => {
-        console.log('[-] An error occured!');
-        console.log(error);
+mongoose
+  .connect(mongodbURI)
+  .then(() => {
+    console.log('[+] Connected to database!');
+    app.listen(appPort, appIP, () => {
+      console.log(`[+] Server started at: http://${appIP}:${appPort}/`);
     });
+  })
+
+  .catch((error) => {
+    console.log('[-] An error occured!');
+    console.log(error);
+  });
--- server/middleware/AppUIConnector.js
+++ server/middleware/AppUIConnector.js
@@ -1,7 +1,6 @@
 import express, { Router } from 'express';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
-import cors from 'cors';
 import path from 'path';
 
 const uiConnector = Router();
@@ -9,30 +8,21 @@ const uiConnector = Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-const corsOptions = {
-    origin: process.env.ACCEPTED_URIS,
-    optionsSuccessStatus: 200,
-    credentials: true,
-};
-
 // connection for the app interface
-uiConnector.use(cors(corsOptions));
-
 // checks for existence of file
 if (fs.existsSync(path.join(__dirname, '..', 'static', 'index.html'))) {
-    console.log('[+] Buildfile exists!');
-    console.log('[*] Adding middleware connection...');
+  console.log('[+] Buildfile exists!');
+  console.log('[*] Adding middleware connection...');
 
-    uiConnector.use(express.static('server/static'));
-    uiConnector.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', 'static', 'index.html'));
-    });
+  uiConnector.use(express.static('server/static'));
+  uiConnector.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'static', 'index.html'));
+  });
 
-    console.log('[+] Middleware UI added!');
+  console.log('[+] Middleware UI added!');
 } else {
-    console.log('[!] "static/index.html" file does not exist!');
-    console.log('[*] Server will start as api mode')
+  console.log('[!] "static/index.html" file does not exist!');
+  console.log('[*] Server will start as api mode');
 }
 
 export default uiConnector;
