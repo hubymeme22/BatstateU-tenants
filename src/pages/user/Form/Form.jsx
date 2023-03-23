787f1c32479d80ee9361c8796a3e78a38cb8824f
gioshwua <reanokennethjoshua11@gmail.com>
2023-03-23 10:47:03 +0800

    server test fetch user side

--- src/pages/user/Form/Form.jsx
+++ src/pages/user/Form/Form.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
-
+import Loader from '@/components/Loader';
+import axios from 'axios';
 function Form() {
-  return <div>Form</div>;
+  React.useEffect(() => {
+    axios
+      .get('http://localhost:5050/api/billing/:username')
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div>
+      Form
+      <Loader />
+    </div>
+  );
 }
 
 export default Form;
