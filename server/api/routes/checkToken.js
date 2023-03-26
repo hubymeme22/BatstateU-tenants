3c34faa9c4a75b212f9f077352f518006b70ebd4
HueHueberry <huby.new1112@gmail.com>
2023-03-26 22:35:50 +0800

    Separate check for admin and student added

--- server/api/routes/checkToken.js
+++ server/api/routes/checkToken.js
@@ -14,4 +14,34 @@ checkToken.post('/', (req, res) => {
     })
 });
 
+// makes sure that the access is admin
+checkToken.post('/admin', (req, res) => {
+    const token = req.body.token;
+    if (token == null)
+        return res.json({valid: false, access: '', error: 'TokenParameterNotSet'});
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
+        if (err) return res.json({valid: false, access: '', error: 'InvalidToken'});
+        if (data.userdata.access != 'admin')
+            return res.json({valid: false, access: data.userdata.access, error: 'TokenNotAdmin'});
+
+        res.json({valid: true, access: data.userdata.access, error: ''});
+    })
+});
+
+// makes sure that the access is student
+checkToken.post('/admin', (req, res) => {
+    const token = req.body.token;
+    if (token == null)
+        return res.json({valid: false, access: '', error: 'TokenParameterNotSet'});
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
+        if (err) return res.json({valid: false, access: '', error: 'InvalidToken'});
+        if (data.userdata.access != 'student')
+            return res.json({valid: false, access: data.userdata.access, error: 'TokenNotStudent'});
+
+        res.json({valid: true, access: data.userdata.access, error: ''});
+    })
+});
+
 export default checkToken;
