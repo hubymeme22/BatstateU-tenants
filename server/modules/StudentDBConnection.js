de8e37239604bf98e0ed0d7e2de1f79e30b13b88
HueHueberry <huby.new1112@gmail.com>
2023-03-28 23:13:02 +0800

    Added functionality for retrieving student billing info

--- server/modules/StudentDBConnection.js
+++ server/modules/StudentDBConnection.js
@@ -1,5 +1,6 @@
 import { MongoDBConnection } from "./DBConnection.js";
 import { Student } from "../models/accounts.js";
+import { Bills } from "../models/bills.js";
 
 // this class is made for student's functionalities
 export class StudentDBConnection extends MongoDBConnection {
@@ -14,6 +15,18 @@ export class StudentDBConnection extends MongoDBConnection {
         Student.findOne({username: srCode})
             .then(this.acceptCallback)
             .catch(this.rejectCallback);
+    }
+
+    // gets all the user's bills
+    getUserBills() {
+        Bills.find({'users.username': this.userTokenData.username})
+            .then(this.acceptCallback).catch(this.rejectCallback);
+    }
+
+    // gets all the user's unpaid bills
+    getUserUnpaidBills() {
+        Bills.find({'$and': [{'users.username': this.userTokenData.username}, {'users.paid': false}]})
+            .then(this.acceptCallback).catch(this.rejectCallback);
     }
 
     // changes the password of this account
