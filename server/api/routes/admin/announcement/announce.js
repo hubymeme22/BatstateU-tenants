5bf581474831c87bba6085f4d7dcf06269ffee0e
HueHueberry <huby.new1112@gmail.com>
2023-03-31 00:15:18 +0800

    Added untested announcement route - Also deleted files that will not be used in the future development
    To be implemented:
      - Announcement database where previous announcements will be stored
      - Deletion of announcements

--- server/api/routes/admin/announcement/announce.js
+++ server/api/routes/admin/announcement/announce.js
@@ -1,19 +1,37 @@
 import { Router } from "express";
 import { postRequestPermission, setJSONPacketFormat } from "../../../../middleware/tokenValidator.js";
 import { AdminMongoDBConnection } from "../../../../modules/AdminDBConnection.js";
+import { sendMail } from "../../../../modules/Mailer.js";
 import paramChecker from "../../../../modules/paramchecker.js";
 
 const announce = Router();
-setJSONPacketFormat({error: '', added: false});
+setJSONPacketFormat({error: '', announcedTo: []});
 
 announce.post('/', postRequestPermission, (req, res) => {
     const missedParams = paramChecker(['subject', 'message'], req.body);
-    const responseFormat = { error: '', announced: false };
+    const responseFormat = { error: '', announcedTo: [] };
 
     if (missedParams.length > 0) {
         responseFormat.error = `missed_params=${missedParams}`;
         return res.json(responseFormat);
     }
+
+    const DBConnection = new AdminMongoDBConnection(req.allowedData);
+    DBConnection.setAcceptCallback(userdata => {
+        const usernameList = Object.keys(userdata);
+        usernameList.forEach(username => {
+            // email the students here
+        });
+
+        // updates the announcement database
+    });
+
+    DBConnection.setRejectCallback(error => {
+        responseFormat.error = error;
+        res.json(responseFormat);
+    });
+
+    DBConnection.retrieveAllStudentData();
 });
 
 export default announce;
--- server/modules/adminRoleChecker.js
+++
@@ -1,7 +0,0 @@
-// Used a ternary operator to check if the user has an admin access based on userTokenData.access 
-export default function adminRoleChecker(userTokenData) {
-    // checks if the value of userTokenData.access is admin if True access granted if not then Insufficientpermission
-    return userTokenData.access === 'admin' ? Promise.resolve('Access granted') : Promise.reject("InssuficientPermission")
-}
-
-// module.exports = adminRoleChecker;
