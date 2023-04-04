69c0ef770daf35eb75dc84236a5a4a0224ce4868
Sanjero20 <gelosantos72@gmail.com>
2023-04-05 04:10:25 +0800

    Revert min-height for whole app

--- src/layouts/main/styled.jsx
+++ src/layouts/main/styled.jsx
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 export const MainContainer = styled.div`
   background-color: ${({ theme }) => theme.darkGray};
   padding: 1em;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   gap: 1em;
-  overflow: hidden;
 
   @media screen and (max-width: 1200px) {
     width: 1300px;
