92c54c2c22121a97e1c8dc8c88eb393c1f291be0
HueHueberry <huby.new1112@gmail.com>
2023-04-08 21:17:36 +0800

    middlename is now not required

--- server/models/accounts.js
+++ server/models/accounts.js
@@ -25,7 +25,6 @@ const adminAccountSchema = mongoose.Schema({
         },
         middle: {
             type: String,
-            required: true
         },
         last: {
             type: String,
