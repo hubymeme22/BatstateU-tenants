b0fd2525d8b54e6a3310a7219d775a20ce07602f
gioshwua <reanokennethjoshua11@gmail.com>
2023-04-09 13:05:54 +0800

    form validation, forgot password

--- src/pages/user/ForgotPass/components/ResetPassword.jsx
+++ src/pages/user/ForgotPass/components/ResetPassword.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { pinInput } from '../../../../services/request';
+import styled from 'styled-components';
 import {
   LoginContainer,
   Form,
@@ -13,15 +14,33 @@ import {
 } from '../styled';
 import BackgroundPath from '@/assets/background.webp';
 
-function ResetPassword() {
+function ResetPassword(props) {
   const navigate = useNavigate();
+  const [code, setCode] = React.useState(props.my);
   const [pass, setPass] = React.useState('');
+  const [confirm, setConfirm] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await pinInput({ pin });
-    alert('Thankyou! you will receive a code in your email shortly!');
+    const V = {
+      pass: /^[\w]{8,20}$/gi,
+      confirm: /^[\w]{8,20}$/gi,
+    };
+    if (pass.length <= 7) {
+      setMessage('Password too Short!!');
+    } else if (!V.pass.test(pass)) {
+      setMessage('Invalid Password');
+    } else if (!V.confirm.test(confirm)) {
+      setMessage('Invalid Password');
+    } else if (pass !== confirm) {
+      setMessage("Password Don't Match!");
+    } else {
+      const response = await pinInput({ pin });
+      alert('Thankyou! you will receive a code in your email shortly!');
 
-    console.log(response);
+      console.log(response);
+    }
   };
 
   return (
@@ -36,11 +55,22 @@ function ResetPassword() {
               type='password'
               id='pass'
               name='pass'
-              placeholder='Use strong password!'
+              placeholder='Input Password'
               onChange={(e) => setPass(e.target.value)}
             />
             <UserIcon />
           </div>
+          <div>
+            <Input
+              type='password'
+              id='pass'
+              name='confirm'
+              placeholder='Confirm Password'
+              onChange={(e) => setConfirm(e.target.value)}
+            />
+            <UserIcon />
+          </div>
+          <Message> {message}</Message>
           <Button> Reset Password </Button>
         </Field>
       </Form>
@@ -48,3 +78,9 @@ function ResetPassword() {
   );
 }
 export default ResetPassword;
+const Message = styled.p`
+  font-size: 12px;
+  text-align: center;
+  color: red;
+  font-weight: bold;
+`;
