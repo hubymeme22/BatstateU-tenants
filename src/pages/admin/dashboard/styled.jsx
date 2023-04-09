cb7ecc41d47437e22b30892313a238f601acb48b
Sanjero20 <gelosantos72@gmail.com>
2023-04-10 03:18:10 +0800

    Add drop shadow

--- src/pages/admin/dashboard/styled.jsx
+++ src/pages/admin/dashboard/styled.jsx
@@ -10,6 +10,12 @@ export const Layout = styled.div`
 
   gap: 1rem;
 
+  & > div {
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em,
+      rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em,
+      rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
+  }
+
   /* Dormitory */
   div:nth-child(1) {
     grid-row: 1 / 3;
