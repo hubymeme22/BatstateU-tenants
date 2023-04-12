345f6dfcaa5fdf8df04201e0ec1f330b2b158e33
Sanjero20 <gelosantos72@gmail.com>
2023-04-12 23:38:28 +0800

    Add announcement display

--- src/pages/admin/announcement/Announcement.jsx
+++ src/pages/admin/announcement/Announcement.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import { Title, Container, Subject, Message, Button } from './styled';
+
+import useInput from '../../../hooks/useInput';
+
 function Announcement() {
-  return <div>Announcement</div>;
+  const [subject, subjectHandler, resetSubject] = useInput('');
+  const [message, messageHandler, resetMessage] = useInput('');
+
+  const sendAnnouncement = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Title>Announcement</Title>
+
+      <Container onSubmit={(e) => sendAnnouncement(e)}>
+        <Subject placeholder="Subject" {...subjectHandler} required />
+
+        <Message placeholder="Message" {...messageHandler} required></Message>
+
+        <Button color=""> Send </Button>
+      </Container>
+    </div>
+  );
 }
 
 export default Announcement;
--- src/pages/admin/announcement/styled.jsx
+++ src/pages/admin/announcement/styled.jsx
@@ -1,1 +1,47 @@
 import styled from 'styled-components';
+
+import { Button as Btn } from '../../../styles/shared/button';
+import { theme } from '../../../styles/theme';
+
+export const Title = styled.h1`
+  margin: 0 auto;
+
+  text-align: center;
+
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+export const Container = styled.form`
+  margin: 0 auto;
+  width: min(100%, 1000px);
+  background-color: ${theme.gray};
+  min-height: 50%;
+  padding: 2rem;
+  padding-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  border-radius: 10px;
+
+  input,
+  textarea {
+    border-radius: 5px;
+    border: 0;
+    padding: 0.5rem;
+  }
+`;
+
+export const Subject = styled.input`
+  width: 40%;
+`;
+
+export const Message = styled.textarea`
+  height: 300px;
+  resize: none;
+`;
+
+export const Button = styled(Btn)`
+  background-color: ${theme.green};
+`;
