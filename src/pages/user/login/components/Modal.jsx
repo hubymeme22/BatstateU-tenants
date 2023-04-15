8ee822c7d0e99408c49892b7b25c0cc7070a8e8b
gioshwua <reanokennethjoshua11@gmail.com>
2023-04-16 01:19:33 +0800

    TAC update

--- src/pages/user/login/components/Modal.jsx
+++ src/pages/user/login/components/Modal.jsx
@@ -392,6 +392,95 @@ function Modal({ handleClick }) {
           convenience to you and you access and use them entirely at your own
           risk and subject to such third parties' terms and conditions.
         </Content>
+        <br /> <Bold color='red'> Terms and Conditions </Bold> <br />
+        <Content>Welcome to batstateu-tenants!</Content>
+        <Content>
+          These terms and conditions outline the rules and regulations for the
+          use of RGO-Alangilan's Website, located at www.batstateu-tenants.com.
+        </Content>
+        <br />
+        <Content>
+          By accessing this website we assume you accept these terms and
+          conditions. Do not continue to use batstateu-tenants if you do not
+          agree to take all of the terms and conditions stated on this page.
+        </Content>
+        <br />
+        <Content>
+          The following terminology applies to these Terms and Conditions,
+          Privacy Statement and Disclaimer Notice and all Agreements: "Client",
+          "You" and "Your" refers to you, the person log on this website and
+          compliant to the Company's terms and conditions. "The Company",
+          "Ourselves", "We", "Our" and "Us", refers to our Company. "Party",
+          "Parties", or "Us", refers to both the Client and ourselves. All terms
+          refer to the offer, acceptance and consideration of payment necessary
+          to undertake the process of our assistance to the Client in the most
+          appropriate manner for the express purpose of meeting the Client's
+          needs in respect of provision of the Company's stated services, in
+          accordance with and subject to, prevailing law of ph. Any use of the
+          above terminology or other words in the singular, plural,
+          capitalization and/or he/she or they, are taken as interchangeable and
+          therefore as referring to same.
+        </Content>
+        <br />
+        <Bold> Cookies</Bold> <br />
+        <Content>
+          We employ the use of cookies. By accessing batstateu-tenants, you
+          agreed to use cookies in agreement with the RGO-Alangilan's Privacy
+          Policy. Most interactive websites use cookies to let us retrieve the
+          user's details for each visit. Cookies are used by our website to
+          enable the functionality of certain areas to make it easier for people
+          visiting our website. Some of our affiliate/advertising partners may
+          also use cookies.
+        </Content>
+        <br /> <Bold>License</Bold> <br />
+        <Content>
+          Unless otherwise stated, RGO-Alangilan and/or its licensors own the
+          intellectual property rights for all material on batstateu-tenants.
+          All intellectual property rights are reserved. You may access this
+          from batstateu-tenants for your own personal use subjected to
+          restrictions set in these terms and conditions.
+          <br />
+          You must not: <br />
+          • Republish material from batstateu-tenants
+          <br />
+          • Sell, rent or sub-license material from batstateu-tenants
+          <br />
+          • Reproduce, duplicate or copy material from batstateu-tenants
+          <br />• Redistribute content from batstateu-tenants
+          <Content>
+            This Agreement shall begin on the date hereof. Our Terms and
+            Conditions were created with the help of the Terms and Conditions
+            Generator. Parts of this website offer an opportunity for users to
+            post and exchange opinions and information in certain areas of the
+            website. RGO-Alangilan does not filter, edit, publish or review
+            Comments prior to their presence on the website. Comments do not
+            reflect the views and opinions of RGO-Alangilan,its agents and/or
+            affiliates. Comments reflect the views and opinions of the person
+            who post their views and opinions. To the extent permitted by
+            applicable laws, RGO-Alangilan shall not be liable for the Comments
+            or for any liability, damages or expenses caused and/or suffered as
+            a result of any use of and/or posting of and/or appearance of the
+            Comments on this website. RGO-Alangilan reserves the right to
+            monitor all Comments and to remove any Comments which can be
+            considered inappropriate, offensive or causes breach of these Terms
+            and Conditions.
+          </Content>{' '}
+          <br />
+          You warrant and represent that:
+          <br />
+          •You are entitled to post the Comments on our website and have all
+          necessary licenses and consents to do so; •The Comments do not invade
+          any intellectual property right, including without limitation
+          copyright, patent or trademark of any third party; •The Comments do
+          not contain any defamatory, libelous, offensive, indecent or otherwise
+          unlawful material which is an invasion of privacy •The Comments will
+          not be used to solicit or promote business or custom or present
+          commercial activities or unlawful activity.
+          <br />
+          You hereby grant RGO-Alangilan a non-exclusive license to use,
+          reproduce, edit and authorize others to use, reproduce and edit any of
+          your Comments in any and all forms, formats or media.
+        </Content>
         <br />
         <Buttons onClick={handleClick} required>
           I ACCEPT ALL THE TERMS AND CONDITIONS
@@ -439,6 +528,7 @@ const Buttons = styled(Button)`
   font-size: 12px;
   width: auto;
   padding: 10px;
+  font-weight: bold; ;
 `;
 const Title = styled.div`
   color: #c52d2d;
@@ -457,6 +547,7 @@ const Bold = styled.p`
   font-weight: bold;
   font-size: 15px;
   text-align: center;
+  color: ${(props) => (props.color === 'red' ? 'red' : 'black')};
 `;
 
 // overlay: {
