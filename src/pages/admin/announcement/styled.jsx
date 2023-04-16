c17f54e16107ad994f4378233e86232729d1dedc
Sanjero20 <gelosantos72@gmail.com>
2023-04-16 23:56:39 +0800

    Add drop-shadow on annoucement container

--- src/pages/admin/announcement/styled.jsx
+++ src/pages/admin/announcement/styled.jsx
@@ -24,6 +24,9 @@ export const Container = styled.form`
   flex-direction: column;
   gap: 1rem;
   border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em,
+    rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em,
+    rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
 
   input,
   textarea {
