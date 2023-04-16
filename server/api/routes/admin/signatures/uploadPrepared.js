d7d7e0ce6dd804de6fe436db8bda0e9eae180dfc
HueHueberry <huby.new1112@gmail.com>
2023-04-16 18:58:39 +0800

    added signature upload for prepared and verified

---
+++ server/api/routes/admin/signatures/uploadPrepared.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { getRequestPermission } from "../../../../middleware/tokenValidator.js";
+import { pngWriteFilter } from '../../../../modules/pngImageFilter.js';
+
+const uploadPrepared = Router();
+uploadPrepared.post('/prepare', getRequestPermission, (req, res) => {
+    if (!req.files)
+        return res.json({ uploaded: false, error: 'NoFilesRecieved' });
+
+    // retrieves the first file uploaded
+    const fileUploaded = Object.keys(req.files);
+    if (fileUploaded.length <= 0)
+        return res.json({ uploaded: false, error: 'NoFileBytesRecieved' });
+
+    const filedata = req.files[fileUploaded[0]];
+    const fileBuffer = filedata.data;
+    const signatureErrCallback = () => {
+        res.json({ uploaded: false, error: 'SignatureNotPNG' });
+    };
+
+    pngWriteFilter('prepared', fileBuffer, signatureErrCallback, (err) => {
+        if (err) return res.json({ uploaded: false, error: err })
+        res.json({ uploaded: true, error: '' })
+    });
+});
+
+export default uploadPrepared;
