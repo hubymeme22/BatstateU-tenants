2e29c09f200e1e445ad70a51f855ac977fb0dc28
gioshwua <reanokennethjoshua11@gmail.com>
2023-04-16 18:33:41 +0800

    added TAC contents

--- src/pages/user/login/components/Modal.jsx
+++ src/pages/user/login/components/Modal.jsx
@@ -482,6 +482,75 @@ function Modal({ handleClick }) {
           your Comments in any and all forms, formats or media.
         </Content>
         <br />
+        <br />
+        <Bold>Hyperlinking to our Content</Bold>
+        <br />
+        <Content>
+          The following organizations may link to our Website without prior
+          written approval: <br />
+          •Government agencies;
+          <br />
+          •Search engines;
+          <br />
+          • News organizations;
+          <br />
+          • Online directory distributors may link to our Website in the same
+          manner as they hyperlink to the Websites of other listed businesses;
+          and
+          <br />• System wide Accredited Businesses except soliciting non-profit
+          organizations, charity shopping malls, and charity fundraising groups
+          which may not hyperlink to our Web site.
+        </Content>{' '}
+        <br />
+        <Content>
+          These organizations may link to our home page, to publications or to
+          other Website information so long as the link: (a) is not in any way
+          deceptive; (b) does not falsely imply sponsorship, endorsement or
+          approval of the linking party and its products and/or services; and
+          (c) fits within the context of the linking party's site. We may
+          consider and approve other link requests from the following types of
+          organizations: <br />
+          • commonly-known consumer and/or business information sources;
+          <br />
+          • dot.com community sites;
+          <br />
+          • associations or other groups representing charities;
+          <br />
+          • online directory distributors;
+          <br />
+          • internet portals;
+          <br />
+          • accounting, law and consulting firms; and
+          <br />
+          • educational institutions and trade associations.
+          <br />
+        </Content>
+        <br />
+        <Content>
+          We will approve link requests from these organizations if we decide
+          that: (a) the link would not make us look unfavorably to ourselves or
+          to our accredited businesses; (b) the organization does not have any
+          negative records with us; (c) the benefit to us from the visibility of
+          the hyperlink compensates the absence of RGO-Alangilan; and (d) the
+          link is in the context of general resource information.
+        </Content>
+        <br />
+        <Content>
+          These organizations may link to our home page so long as the link: (a)
+          is not in any way deceptive; (b) does not falsely imply sponsorship,
+          endorsement or approval of the linking party and its products or
+          services; and (c) fits within the context of the linking party's site.
+        </Content>
+        <br />
+        <Content>
+          If you are one of the organizations listed in paragraph 2 above and
+          are interested in linking to our website, you must inform us by
+          sending an e-mail to RGO-Alangilan. Please include your name, your
+          organization name, contact information as well as the URL of your
+          site, a list of any URLs from which you intend to link to our Website,
+          and a list of the URLs on our site to which you would like to link.
+          Wait 2-3 weeks for a response.
+        </Content>
         <Buttons onClick={handleClick} required>
           I ACCEPT ALL THE TERMS AND CONDITIONS
         </Buttons>
